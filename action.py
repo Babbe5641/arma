d5f2553c6f98398fe9a70d53700a3ad99001e812
SirLexy564 <dukenukemer@gmail.com>
2026-08-06 00:45:56 +0100

    workflow test

---
+++ action.py
@@ -0,0 +1,35 @@
+import time
+
+import a2s
+import requests
+
+tg = '8249005370:AAEpc0Tm9MnbAT1v9aCTlDUkZr9X1Qreeq4'
+tg_id = '5633643414'
+
+
+def query():
+    try:
+        if a2s.info(("165.217.129.29", 9121)).player_count < 20:
+            return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def send(text):
+    url = f"https://api.telegram.org/bot{tg}/sendMessage"
+    msg = {
+        "chat_id": tg_id,
+
+    }
+    msg["text"] = text
+    r = requests.post(url, json=msg)
+    # print(r.status_code)
+    # print(r.text)
+if __name__ == "__main__":
+    send("Online")
+    while True:
+        if query():
+            send("LOW POP")
+            time.sleep(300)
+        time.sleep(10)
